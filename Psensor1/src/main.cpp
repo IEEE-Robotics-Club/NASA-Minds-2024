35205e5effc4e15a4fb2b40065bc82f259b76382
nriley1 <89812871+nriley1@users.noreply.github.com>
2024-03-08 09:49:42 -0700

    Initial commit

---
+++ Psensor1/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(A9);
+  float voltage = sensorValue * (3.3 / 1024);
+  Serial.println(voltage);
+}
