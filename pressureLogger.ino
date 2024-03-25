c0d80dbbc33ac7cbf2cfe42803249197ddddcc1a
Jacob Fifield <43118015+Fife@users.noreply.github.com>
2024-03-25 09:24:10 -0600

    Update pressureLogger.ino

--- pressureLogger.ino
+++ pressureLogger.ino
@@ -1,56 +1,18 @@
-/*
-  SD card datalogger
- 
- This example shows how to log data from three analog sensors
- to an SD card using the SD library.
- 	
- The circuit:
- * analog sensors on analog ins 0, 1, and 2
- * SD card attached to SPI bus as follows:
- ** MOSI - pin 11, pin 7 on Teensy with audio board
- ** MISO - pin 12
- ** CLK - pin 13, pin 14 on Teensy with audio board
- ** CS - pin 4,  pin 10 on Teensy with audio board
- 
- created  24 Nov 2010
- modified 9 Apr 2012
- by Tom Igoe
- 
- This example code is in the public domain.
- */
-
 #include <SD.h>
 #include <SPI.h>
 #include <string.h>
 
-// On the Ethernet Shield, CS is pin 4. Note that even if it's not
-// used as the CS pin, the hardware CS pin (10 on most Arduino boards,
-// 53 on the Mega) must be left as an output or the SD library
-// functions will not work.
-
-// change this to match your SD shield or module;
-// Arduino Ethernet shield: pin 4
-// Adafruit SD shields and modules: pin 10
-// Sparkfun SD shield: pin 8
-// Teensy audio board: pin 10
-// Teensy 3.5 & 3.6 & 4.1 on-board: BUILTIN_SDCARD
-// Wiz820+SD board: pin 4
-// Teensy 2.0: pin 0
-// Teensy++ 2.0: pin 20
 const int chipSelect = BUILTIN_SDCARD;
 const int analogPin = 9;
-
 String logFileName; 
 
 void setup()
 {
-
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect.
   }
-
 
   Serial.print("Initializing SD card...");
   
@@ -64,6 +26,7 @@ void setup()
   Serial.println("card initialized.");
   logFileName = createFileName();
 
+  //Debug Printng
   //printDirectory(root, 0);
 }
 
